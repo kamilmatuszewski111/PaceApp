e6ee6922ee7ebb035e7ce8c499f56228a37f6bcd
kamilmatuszewski111 <kamilmatuszewski95@gmail.com>
2025-03-28 00:05:25 +0100

    Add back to main screen

--- main.py
+++ main.py
@@ -32,38 +32,47 @@ class PaceApp(App):
         return self.main_screen()
 
     def main_screen(self):
-
         self.layout = BoxLayout(orientation='vertical')
 
-        self.select_speed_button = Button(text='Calculate speed')
+        self.select_speed_button = Button(text='Calculate speed - basis on pace and time')
         self.select_speed_button.bind(on_press=self.speed_screen)
         self.layout.add_widget(self.select_speed_button)
 
-        self.select_pace_button = Button(text='Calculate distance')
+        self.select_pace_button = Button(text='Calculate distance - basis on pace and time')
         self.select_pace_button.bind(on_press=self.distance_screen)
         self.layout.add_widget(self.select_pace_button)
 
-
-
         return self.layout
+
+    def back_to_main_screen(self, instance):
+        self.layout.clear_widgets()
+
+        self.main_screen()
+
+        Window.clear()
+        Window.add_widget(self.layout)
 
     def speed_screen(self, instance):
         self.layout.clear_widgets()
 
         self.layout = BoxLayout(orientation='vertical')
 
-        self.time_input = TextInput(hint_text='Podaj czas (hh:mm:ss)')
+        self.time_input = TextInput(hint_text='Time (hh:mm:ss)')
         self.layout.add_widget(self.time_input)
 
-        self.pace_input = TextInput(hint_text='Podaj tempo (mm:ss)')
+        self.pace_input = TextInput(hint_text='Pace (mm:ss)')
         self.layout.add_widget(self.pace_input)
 
-        self.result_label_speed = Label(text='Prędkośc na podstawie tempa oraz czasu')
+        self.result_label_speed = Label(text='Speed basis on pace and time')
         self.layout.add_widget(self.result_label_speed)
 
-        self.calculate_button = Button(text='Oblicz')
+        self.calculate_button = Button(text='Calculate')
         self.calculate_button.bind(on_press=self.calculate_speed)
         self.layout.add_widget(self.calculate_button)
+
+        self.to_main_screen = Button(text='Back to main screen')
+        self.to_main_screen.bind(on_press=self.back_to_main_screen)
+        self.layout.add_widget(self.to_main_screen)
 
         Window.clear()
         Window.add_widget(self.layout)
@@ -73,18 +82,22 @@ class PaceApp(App):
 
         self.layout = BoxLayout(orientation='vertical')
 
-        self.time_input = TextInput(hint_text='Podaj czas (hh:mm:ss)')
+        self.time_input = TextInput(hint_text='Time (hh:mm:ss)')
         self.layout.add_widget(self.time_input)
 
-        self.pace_input = TextInput(hint_text='Podaj tempo (mm:ss)')
+        self.pace_input = TextInput(hint_text='Pace (mm:ss)')
         self.layout.add_widget(self.pace_input)
 
-        self.result_label_distance = Label(text='Dystans na podstawie tempa oraz czasu')
+        self.result_label_distance = Label(text='Distance basis on pace and time')
         self.layout.add_widget(self.result_label_distance)
 
-        self.calculate_button = Button(text='Oblicz')
+        self.calculate_button = Button(text='Calculate')
         self.calculate_button.bind(on_press=self.calculate_distance)
         self.layout.add_widget(self.calculate_button)
+
+        self.to_main_screen = Button(text='Back to main screen')
+        self.to_main_screen.bind(on_press=self.back_to_main_screen)
+        self.layout.add_widget(self.to_main_screen)
 
         Window.clear()
         Window.add_widget(self.layout)
@@ -95,7 +108,7 @@ class PaceApp(App):
 
         try:
             speed = time_pace_to_speed(time, pace)
-            self.result_label_speed.text = f"Średnia prędkość: {speed:.2f} km/h"
+            self.result_label_speed.text = f"Average speed: {speed:.2f} km/h"
         except Exception as e:
             self.result_label_speed.text = f"Wrong data"
 
@@ -105,7 +118,7 @@ class PaceApp(App):
 
         try:
             distance = time_pace_to_distance(time, pace)
-            self.result_label_distance.text = f"Przebyty dystans: {distance:.2f} km"
+            self.result_label_distance.text = f"Distance: {distance:.2f} km"
         except Exception as e:
             self.result_label_distance.text = f"Wrong data"
 
